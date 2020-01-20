13fd3076692402f08bf93a305d394ec5891951c8
jackson <mpostawka@gmail.com>
2020-01-20 18:12:53 +0100

    Initial commit

---
+++ RigidBodySimulation/build.sh
@@ -0,0 +1,3 @@
+nvcc -O3 main.cu -Iinclude/ -o main -lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -lglfw3 -lGLEW
+#-lglfw3 -lGLEW -lGL -lX11
+#-lGL -lX11 -lXi -lXrandr -lXxf86vm -lXinerama -lXcursor -lrt -lm -lglfw3 -lGLEW
